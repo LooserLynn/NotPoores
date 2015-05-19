99a42f200e7a662804f7ee0b5ce5f926b56bf7c0
slinam2209 <slinam2209@001LAB102010531.neisd.ad>
2015-05-19 12:45:19 -0500

    Not

---
+++ Copy of LibrarywInterface/src/Library/Library.java
@@ -0,0 +1,61 @@
+package Library;
+
+import java.util.Scanner;
+
+
+public class Library {
+	static int minCustomers = 100;
+	static Customer[] cust = new Customer [100];
+    static Book[] books = new Book[10];
+	
+	//Create a scanner object each for text and numbers
+	private static Scanner tInput = new Scanner(System.in);
+	private static Scanner nInput = new Scanner(System.in);
+	
+	
+	public static void main(String[] args) {
+		
+		//Add the books
+	/*	addBooks();
+		
+		for (int i = 0; i < books.length; i++ )
+		{
+			System.out.println(books[i].getTitle() + ", by " + books[i].getAuthor());
+		}
+		
+		do 
+		{
+		System.out.println("What is the maximum amount of customers you expect?");
+		} while (!(Integer.parseInt(tInput.nextLine())>=minCustomers));
+		
+
+		
+		*/
+	}
+	
+	private static void addCustomer()
+	{
+		
+		for (int i = 0; i < minCustomers; i++)
+		{
+			System.out.println(cust[i].getID());
+		}
+
+	} 
+	
+	private static void addBooks()
+	{
+		for (int i = 0; i < books.length; i++ )
+		{
+			books[i] = new Book();
+			System.out.println("Enter a book name");
+			books[i].setTitle(tInput.nextLine());
+			System.out.println("the Author?");
+			books[i].setAuthor(tInput.nextLine());
+			
+		}
+		
+
+	}
+
+}
